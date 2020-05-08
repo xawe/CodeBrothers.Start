87b2ae0f2ea88e0e8bc281978edefbe9be2273b2
Alam <dreamxawe@hotmail.com>
2020-05-08 18:17:25 -0300

    adicionado pacote de entity. WIP - necessário implementar insert de usuário com criptografia da senha

---
+++ backend/java/codebrothers.services.auth/src/main/java/com/codebrothers/services/auth/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codebrothers.services.auth.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class UserRepository {
+
+}
+
