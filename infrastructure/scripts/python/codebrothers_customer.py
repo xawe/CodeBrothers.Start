90924554d76a43ffbba920bc8f51175621e51200
Alam <dreamxawe@hotmail.com>
2020-05-25 11:14:42 -0300

    Ajustes em ordem de script
    Devido a implementação teste do feign , caso o customer suba sem ter sido registrado o serviço de auth, ocorre um erro na execução do customer. Dessa forma, alterei o script para subir o auth primeiro, e inclui uma validação de dependencia que verifica se o container esta ativo. Talvez seja necessário também verificar se o container esta registrado no service discover.
    Importante.; Essa alteração deverá não mais ser necessária quando for retirada a chamada do serviço.

--- infrastructure/scripts/python/codebrothers_customer.py
+++ infrastructure/scripts/python/codebrothers_customer.py
@@ -2,6 +2,7 @@ import sys
 import docker
 import subprocess
 import os
+import time
 
 #sys.path.insert(0, './Helpers')
 from Helpers import docker_helper
@@ -16,6 +17,36 @@ customerPath = "/backend/java/codebrothers.services.customer"
 imageName = "codebrothers.customer"
 fullPath = currentPath + customerPath
 port = "8081"
+dependencies = ["codebrothers.auth"]
+
+
+def CheckDependency(output):
+    r = True
+    for dep in dependencies:
+        r = TryCheckContainerActive(output, dep)
+        if(r == False):
+            msg = "O container " + dep + " parece não estar ativo. Verifique se o mesmo está funcionado normalmente e se está registrado no Service Discovery antes de tentar novamente."            
+                        
+            output.writelines(msg)
+            MsgNoBlock(msg)
+            r = False
+
+    return r
+def TryCheckContainerActive(output, dependencia):
+    tentativas = 5
+    segundosBase = 3
+    r = False
+    for t in range(tentativas):
+        tempo = segundosBase * (t+1)
+        r = docker_helper.isContainerActive(dependencia)
+        if(r):
+            r = True
+            break
+        else:
+            MsgNoBlock("Dependência " + dependencia + " não está ativa")
+            MsgNoBlock("Tentaremos mais " + str(tentativas - t) +" vez(es). Aguarando " + str(tempo) + " segundos antes da nova tentativa") 
+            time.sleep(tempo)
+    return r
 
 
 def BuildApp(output):        
@@ -48,19 +79,21 @@ def StartContainer(output):
     MsgNoBlock("Docker Container iniciado com sucesso")
     return retorno
 
-MsgTitle("Iniciando o processo de construção do codebrothers.services.customer")
-
 with open(logPath, "a") as output:        
-    if(BuildApp(output) == 0):
-        if(BuildDockerImage(output) == 0):
-            StartContainer(output)            
-        else:
-            Msg("Ocorreu um erro no Build da imagem Docker. Veja detalhes do erro em " + logPath)
-    else:
-        Msg("Ocorreu um erro no Build da aplicação. Veja detalhes do erro em " + logPath)
-
-
-MsgEnd("Workflow CodeBrother.Services.Customer finalizado")
-#MsgTitle("Workflow CodeBrother.Services.Customer finalizado")        
-
+    MsgTitle("Iniciando o processo de construção do codebrothers.services.customer")
+    Msg("Iniciando a validação de dependências")
+    isDependencyOk = CheckDependency(output)
     
+    if(isDependencyOk == True):        
+        MsgNoBlock("Dependências inicializadas. Prosseguindo o processo")
+        if(BuildApp(output) == 0):
+            if(BuildDockerImage(output) == 0):
+                StartContainer(output)            
+            else:
+                Msg("Ocorreu um erro no Build da imagem Docker. Veja detalhes do erro em " + logPath)
+        else:
+            Msg("Ocorreu um erro no Build da aplicação. Veja detalhes do erro em " + logPath)
+    else:
+        MsgEnd("Processo finalizado prematuramente. Ajuste os problemas e tente novamente. Log >> " + logPath)    
+    MsgEnd("Workflow CodeBrother.Services.Customer finalizado")
+
